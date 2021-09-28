cc55be49eda6a97611d13cfea285c5dc8a5a4c8c
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-09-28 19:55:16 -0300

    first commit

---
+++ app/src/main/java/com/example/platzi_music_design/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.platzi_music_design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
